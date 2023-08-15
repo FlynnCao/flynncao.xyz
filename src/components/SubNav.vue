9d2c9efe9f52cce2e70a99938ecec13119245268
flynncao <flynncao@yahoo.com>
2023-08-15 17:19:07 +0800

    style: eslint configuration

--- src/components/SubNav.vue
+++ src/components/SubNav.vue
@@ -14,7 +14,7 @@ const route = useRoute()
       <div :i="englishOnly ? 'carbon-checkbox-checked' : 'carbon-checkbox'" />
       English Only
     </button>
-    <!--
+
     <div mb-0 flex="~ gap2 sm:gap3 wrap" text-xl sm:text-3xl>
       <RouterLink to="/posts" class="!border-none" w-17 :class="route.path === '/posts' ? activeStyle : inactiveStyle">
         Blog
@@ -22,12 +22,18 @@ const route = useRoute()
       <RouterLink to="/talks" class="!border-none" w-20 :class="route.path === '/talks' ? activeStyle : inactiveStyle">
         Talks
       </RouterLink>
-      <RouterLink to="/podcasts" class="!border-none" w-35 :class="route.path === '/podcasts' ? activeStyle : inactiveStyle">
+      <RouterLink
+        to="/podcasts" class="!border-none" w-35
+        :class="route.path === '/podcasts' ? activeStyle : inactiveStyle"
+      >
         Podcasts
       </RouterLink>
-      <RouterLink to="/streams" class="!border-none" w-40 :class="route.path === '/streams' ? activeStyle : inactiveStyle">
+      <RouterLink
+        to="/streams" class="!border-none" w-40
+        :class="route.path === '/streams' ? activeStyle : inactiveStyle"
+      >
         Streams
       </RouterLink>
-    </div> -->
+    </div>
   </div>
 </template>
